a3201d9c70184b026e31d3b7a6fb67711cb39296
xtreme6007 <prestonnichols96@gmail.com>
2020-09-14 11:50:52 -0500

    added in routes for index and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,15 @@ var PORT = 8080;
 // Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
