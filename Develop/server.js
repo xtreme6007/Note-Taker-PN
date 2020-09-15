388b33a50b90cb881317b1df01822eb88173bc40
xtreme6007 <prestonnichols96@gmail.com>
2020-09-15 11:31:56 -0500

    added in post and delete and routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,69 @@
-// Dependencies 
+// Dependencies
 
+var fs = require("fs");
 var express = require("express");
 var path = require("path");
+var uuidv1 = require("uuid/v1");
 //--------------------------------
-
+// variables 
 var app = express();
 var PORT = 8080;
+
+var notesArray = [];
+
 
 // Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+// route for index.html
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// route for notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+
+    console.log(data);
+
+    res.json(JSON.parse(data));
+  })
 
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+});
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.post("/api/notes", function (req, res) {
+  //newNote = res.body
+  var title = req.body.title;
+  var text = req.body.text;
+  var newNote = { title, text, id: uuidv1() }
+  var pastNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  pastNotes.push(newNote)
+
+  fs.writeFileSync("db/db.json", JSON.stringify(pastNotes));
+
+  res.json(newNote);
+
+})
+
+
+app.delete("/api/notes/:id", function (req, res) {
+  var pastNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  var updateNotes = pastNotes.filter(function(note){
+    return note.id !== req.params.id
+  })
+  fs.writeFileSync("db/db.json", JSON.stringify(updateNotes));
+  res.json({ok: true})
+})
+
+
+
+// listener function
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
